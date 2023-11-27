52afcec3448a50089dc372886ffd29c996880c91
a18851 <a18851@alunos.ipca.pt>
2023-11-27 19:50:48 +0000

    update main + solution

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from hospital_parser import parse_dataset
 
+#region Import
 with open("dataset.txt", "r") as file:
     dataset = file.read()
 
 departments, rooms, beds, patients = parse_dataset(dataset)
 
-print (beds)
+#endregion
+
